cd247cf3287a8e59fa715a5d7d15e1ec12ea7d2e
Adarsh Agrahari <agrahariadarsh68@gmail.com>
2025-04-18 21:34:26 +0530

    com

--- financetracker/src/SignupSignin/index.js
+++ financetracker/src/SignupSignin/index.js
@@ -1,10 +1,17 @@
 import React, { useState } from "react";
+import {useNavigate} from "react-router-dom";
 import Input from "../components/input";
 import Button from "../components/Button";
 import "./styles.css";
-import { auth } from "../firbase"; // Import the firebase configuration
+import { doc, setDoc, provider } from "../firbase";
+import { getDoc } from "firebase/firestore";
+import { auth, db } from "../firbase"; // Import the firebase configuration
 import { toast } from "react-toastify";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const SignupSigninComponent = () => {
   const [name, setName] = useState("");
@@ -12,6 +19,9 @@ const SignupSigninComponent = () => {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [loading, setLoading] = useState(false);
+  const [loginForm, setLoginForm] = useState(false);
+  const [flag, setFlag] = useState(false);
+  const navigate = useNavigate();
 
   function signupWithEmail() {
     setLoading(true);
@@ -34,6 +44,14 @@ const SignupSigninComponent = () => {
             console.log("user>>>", user);
             toast.success("User Created Successfully");
             setLoading(false);
+            setConfirmPassword("");
+            setName("");
+            setEmail("");
+            setPassword("");
+            createDoc(user);
+            navigate("/dashboard");
+
+            //create A doc with user id as the following id
           })
           .catch((error) => {
             const errorCode = error.code;
@@ -50,46 +68,166 @@ const SignupSigninComponent = () => {
     }
   }
 
+  async function createDoc(user) {
+    setLoading(true);
+    //make sure that the doc with uid does'nt exist
+    if (!user) return;
+
+    const useRef = doc(db, "users", user.uid);
+    const userData = await getDoc(useRef);
+
+    if (!userData.exists()) {
+      try {
+        await setDoc(useRef,{
+          name: user.displayName ? user.displayName : name,
+          email: user.email,
+          photoURL: user.photoURL ? user.photoURL : "",
+          createdAt: new Date().toISOString(),
+        });
+        toast.success("Doc Created");
+        setLoading(false);
+      } catch (e) {
+        toast.error(e.message);
+      }
+    }else{
+        toast.error("Doc already exists");
+    }
+  }
+
+  function loginUsingEmail() {
+    console.log("Email", email);
+    console.log("password", password);
+     setLoading(true);
+    if (email !== "" && password !== "") {
+      signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          // ...
+          toast.success("User Logged In!");
+          console.log("User Logged in", user);
+          setLoading(false);
+          navigate("/dashboard");
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setLoading(false);
+          toast.error(errorMessage);
+        });
+    } else {
+      toast.error("All fields are mandatory!");
+      setLoading(false);
+    }
+  }
+
+  function googleAuth(){
+    signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    // IdP data available using getAdditionalUserInfo(result)
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
+  }
+
   return (
-    <div className="signup-wrapper">
-      <h2 className="title">
-        Sign Up on <span style={{ color: "var(--theme)" }}>Financely.</span>
-      </h2>
-      <form>
-        <Input
-          label={"Full Name"}
-          state={name}
-          setState={setName}
-          placeholder={"John Doe"}
-        />
-        <Input
-          label={"Email"}
-          state={email}
-          setState={setEmail}
-          placeholder={"JohnDoe@gmail.com"}
-        />
-        <Input
-          type="password"
-          label={"Password"}
-          state={password}
-          setState={setPassword}
-          placeholder={"Example@123"}
-        />
-        <Input
-          type="password"
-          label={"Confirm Password"}
-          state={confirmPassword}
-          setState={setConfirmPassword}
-          placeholder={"Example@123"}
-        />
-        <Button
-          text={"Sighnup Using Email and Password"}
-          onClick={signupWithEmail}
-        />
-        <p style={{ textAlign: "center", margin: "0" }}>or</p>
-        <Button text={"Signup Using Google"} blue={true} />
-      </form>
-    </div>
+    <>
+      {loginForm ? (
+        <div className="signup-wrapper">
+          <h2 className="title">
+            Login on <span style={{ color: "var(--theme)" }}>Financely.</span>
+          </h2>
+          <form>
+            <Input
+              label={"Email"}
+              state={email}
+              setState={setEmail}
+              placeholder={"JohnDoe@gmail.com"}
+            />
+            <Input
+              type="password"
+              label={"Password"}
+              state={password}
+              setState={setPassword}
+              placeholder={"Example@123"}
+            />
+            <Button
+              disabled={loading}
+              style={{ textAlign: "center" }}
+              text={loading ? "Loading..." : "Login Using Email and Password"}
+              onClick={loginUsingEmail}
+            />
+            <p className="p-login">or</p>
+            <Button 
+            onClick={googleAuth}
+            text={loading ? "Loading...." : "Login Using Google"} blue={true} />
+            <p className="p-login" onClick={() => setLoginForm(!loginForm)}>
+              Or Don't Have An Account? Click Here
+            </p>
+          </form>
+        </div>
+      ) : (
+        <div className="signup-wrapper">
+          <h2 className="title">
+            Sign Up on <span style={{ color: "var(--theme)" }}>Financely.</span>
+          </h2>
+          <form>
+            <Input
+              label={"Full Name"}
+              state={name}
+              setState={setName}
+              placeholder={"John Doe"}
+            />
+            <Input
+              label={"Email"}
+              state={email}
+              setState={setEmail}
+              placeholder={"JohnDoe@gmail.com"}
+            />
+            <Input
+              type="password"
+              label={"Password"}
+              state={password}
+              setState={setPassword}
+              placeholder={"Example@123"}
+            />
+            <Input
+              type="password"
+              label={"Confirm Password"}
+              state={confirmPassword}
+              setState={setConfirmPassword}
+              placeholder={"Example@123"}
+            />
+            <Button
+              disabled={loading}
+              style={{ textAlign: "center" }}
+              text={loading ? "Loading..." : "Sighnup Using Email and Password"}
+              onClick={signupWithEmail}
+            />
+            <p className="p-login">or</p>
+            <Button 
+            onClick={googleAuth}
+            text={"Signup Using Google"} blue={true} />
+            <p className="p-login" onClick={() => setLoginForm(true)}>
+              Or Have An Account Already? Click Here
+            </p>
+          </form>
+        </div>
+      )}
+    </>
   );
 };
 
--- financetracker/src/components/Button/index.js
+++ financetracker/src/components/Button/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import './styles.css';
 
-const Button = ({text, onClick, blue}) => {
+const Button = ({text, onClick, blue, disabled}) => {
   return (
-    <div className={blue ? "btn btn-blue" : "btn"} onClick={onClick}>
+    <div className={blue ? "btn btn-blue" : "btn"} onClick={onClick}
+    disabled={disabled} style={{cursor: disabled ? 'not-allowed' : 'pointer'}}> 
         {text}
     </div>
   )
