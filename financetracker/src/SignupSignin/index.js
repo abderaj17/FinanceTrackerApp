7fb4391bea4e031abfb601dea18917cf098b63cb
Adarsh Agrahari <agrahariadarsh68@gmail.com>
2025-04-19 21:56:04 +0530

    com

--- financetracker/src/SignupSignin/index.js
+++ financetracker/src/SignupSignin/index.js
@@ -122,25 +122,35 @@ const SignupSigninComponent = () => {
   }
 
   function googleAuth(){
-    signInWithPopup(auth, provider)
-  .then((result) => {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential.accessToken;
-    // The signed-in user info.
-    const user = result.user;
-    // IdP data available using getAdditionalUserInfo(result)
-    // ...
-  }).catch((error) => {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.customData.email;
-    // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
-    // ...
-  });
+    setLoading(true);
+    try {
+        signInWithPopup(auth, provider)
+        .then((result) => {
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          const credential = GoogleAuthProvider.credentialFromResult(result);
+          const token = credential.accessToken;
+          // The signed-in user info.
+          const user = result.user;
+            console.log("User", user);
+            createDoc(user);
+            setLoading(false);
+            navigate("/dashboard");
+            toast.success('User Authenticated');
+          // IdP data available using getAdditionalUserInfo(result)
+          // ...
+        }).catch((error) => {
+          // Handle Errors here.
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          toast.error(errorMessage);
+          // ...
+        });
+    } catch (e) {
+        setLoading(false);
+        toast.error(e.message);
+
+    }
+   
   }
 
   return (
@@ -220,7 +230,7 @@ const SignupSigninComponent = () => {
             <p className="p-login">or</p>
             <Button 
             onClick={googleAuth}
-            text={"Signup Using Google"} blue={true} />
+            text={loading ? "Loading..." : "Signup Using Google"} blue={true} style={{textAlign:"center"}}/>
             <p className="p-login" onClick={() => setLoginForm(true)}>
               Or Have An Account Already? Click Here
             </p>
