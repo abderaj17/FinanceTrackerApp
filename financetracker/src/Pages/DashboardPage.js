f0b80537bba10c08859a26d91c0ac20e39cba2df
Adarsh Agrahari <agrahariadarsh68@gmail.com>
2025-04-19 22:51:48 +0530

    authentication completed

--- financetracker/src/Pages/DashboardPage.js
+++ financetracker/src/Pages/DashboardPage.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Header from '../components/Header'
 
 const DashboardPage = () => {
   return (
-    <div>DashboardPage</div>
+    <div>
+        <Header />
+    </div>
   )
 }
 
--- financetracker/src/components/Header/index.js
+++ financetracker/src/components/Header/index.js
@@ -1,13 +1,42 @@
 import React from 'react'
 import './styles.css';
+import {useAuthState} from 'react-firebase-hooks/auth';
+import {auth} from '../../firbase'
+import { useEffect } from 'react';
+import {useNavigate} from 'react-router-dom';
+import {signOut} from 'firebase/auth';
+import {toast} from 'react-toastify';
+
 
 const Header = () => {
+    const [user, loading] = useAuthState(auth);
+    const navigate = useNavigate();
+
+    useEffect(() =>{
+        if(user){
+        navigate("/dashboard");
+        }
+    }, [user, loading])
+
+    function logoutFnc(){
+        try {
+            signOut(auth).then(() =>{
+                toast.success("Logged out Successfully!");
+                navigate("/");
+            })
+         .catch ((error) => {
+          toast.error(error.message);
+        });
+    }catch(e){
+        toast.error(e.message);
+    }
+}
   return (
     <div className="navbar">
         <p className="logo">Financely.</p>
-        <p className="logo link" >Logout</p>
+        <p className="logo link" onClick={logoutFnc} >Logout</p>
     </div>
   )
 }
 
-export default Header
+export default Header;
