5293876aadc2239f3be5cc91e40d6fa563bd3588
Adarsh Agrahari <agrahariadarsh68@gmail.com>
2025-04-20 22:47:02 +0530

    com

--- financetracker/src/components/Cards/index.js
+++ financetracker/src/components/Cards/index.js
@@ -7,18 +7,23 @@ const Cards = () => {
   return (
     <div>
         <Row className="my-row">
-            <Card className="my-card" title="Current Balance">
-               <p>Rs-0</p>
-               <Button text="Reset Balance" blue={true}/>
-                </Card>
-            <Card className="my-card" title="Current Balance">
-               <p>Rs-0</p>
-               <Button text="Reset Balance" blue={true}/>
-                </Card>
-            <Card className="my-card" title="Current Balance">
-               <p>Rs-0</p>
-               <Button text="Reset Balance" blue={true}/>
-                </Card>
+        <Card bordered={true} className="my-card">
+          <h2>Current Balance</h2>
+          <p>₹0</p>
+         <Button text="Reset Balance" blue={true}/>
+
+        </Card>
+        <Card bordered={true} className="my-card">
+          <h2>Total Income</h2>
+          <p>₹0</p>
+         <Button text="Add Income" blue={true}/>
+          </Card>
+
+          <Card bordered={true} className="my-card">
+            <h2>Total Expenses</h2>
+            <p>₹0</p>
+            <Button text="Add Expense" blue={true}/>
+            </Card>
         </Row>
     </div>
   )
