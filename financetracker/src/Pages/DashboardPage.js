b34f0d1c7ca9ecd8b44a225afdc5f60f4f0512cb
Adarsh Agrahari <agrahariadarsh68@gmail.com>
2025-04-21 20:51:12 +0530

    com

--- financetracker/src/Pages/DashboardPage.js
+++ financetracker/src/Pages/DashboardPage.js
@@ -1,14 +1,47 @@
-import React from 'react'
-import Cards from '../components/Cards'
-import Header from '../components/Header'
+import React, { useState } from "react";
+import Cards from "../components/Cards";
+import Header from "../components/Header";
+import { Modal } from "antd";
 
 const DashboardPage = () => {
+  const [isExpenseModalVisible, setIsExpenseModalVisible] = useState(false);
+  const [isIncomeModalVisible, setIsIncomeModalVisible] = useState(false);
+
+  const showExpenseModal = () => {
+    setIsExpenseModalVisible(true);
+  };
+  const showIncomeModal = () => {
+    setIsIncomeModalVisible(true);
+  };
+  const handleExpenseCancel = () => {
+    setIsExpenseModalVisible(false);
+  };
+  const handleIncomeCancel = () => {
+    setIsIncomeModalVisible(false);
+  };
   return (
     <div>
-        <Header />
-        <Cards />
+      <Header />
+      <Cards
+        showExpenseModal={showExpenseModal}
+        showIncomeModal={showIncomeModal}
+      />
+      <Modal 
+      style={{fontWeight: 600}}
+      title="Add Expense"
+      visible={isIncomeModalVisible}
+      onCancel={handleIncomeCancel}
+      footer={null}
+      >Income</Modal>
+      <Modal 
+      style={{fontWeight: 600}}
+      title="Add Income"
+      visible={isExpenseModalVisible}
+      onCancel={handleExpenseCancel}
+      footer={null}  
+      >Expense</Modal>
     </div>
-  )
-}
+  );
+};
 
-export default DashboardPage; 
+export default DashboardPage;
--- financetracker/src/components/Cards/index.js
+++ financetracker/src/components/Cards/index.js
@@ -3,7 +3,7 @@ import './styles.css';
 import {Card, Row} from 'antd';
 import Button from '../Button';
 
-const Cards = () => {
+const Cards = ({showExpenseModal, showIncomeModal}) => {
   return (
     <div>
         <Row className="my-row">
@@ -16,13 +16,13 @@ const Cards = () => {
         <Card bordered={true} className="my-card">
           <h2>Total Income</h2>
           <p>₹0</p>
-         <Button text="Add Income" blue={true}/>
+         <Button text="Add Income" blue={true} onClick={showIncomeModal}/>
           </Card>
 
           <Card bordered={true} className="my-card">
             <h2>Total Expenses</h2>
             <p>₹0</p>
-            <Button text="Add Expense" blue={true}/>
+            <Button text="Add Expense" blue={true} onClick={showExpenseModal}/>
             </Card>
         </Row>
     </div>
