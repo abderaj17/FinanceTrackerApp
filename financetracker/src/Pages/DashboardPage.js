50c9a6d80a4ea3ecc982c1324e2cb1b6c4900f9b
Adarsh Agrahari <agrahariadarsh68@gmail.com>
2025-04-22 21:15:02 +0530

    com

--- financetracker/src/Pages/DashboardPage.js
+++ financetracker/src/Pages/DashboardPage.js
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
 import Cards from "../components/Cards";
 import Header from "../components/Header";
-import { Modal } from "antd";
+// import { Modal } from "antd";
+import {useAuthState} from "react-firebase-hooks/auth";
+import {addDoc, collection} from "firebase/firestore"
+import {auth, db} from "../firbase";
 import AddExpenseModal from "../components/Modals/addExpense";
 import AddIncomeModal from "../components/Modals/addIncome";
 
 const DashboardPage = () => {
+  const [user] = useAuthState(auth);
   const [isExpenseModalVisible, setIsExpenseModalVisible] = useState(false);
   const [isIncomeModalVisible, setIsIncomeModalVisible] = useState(false);
 
@@ -23,7 +27,31 @@ const DashboardPage = () => {
   };
 
   const onFinish = (values, type) => {
-    console.log("On finish", values, type);
+   const newTransaction= {
+    type:type,
+    date:moment(values.date).format("YYYY-MM-DD"),
+    amount: parseFloat(values.amount),
+    tag: values.tag,
+    name: values.name,
+   };
+   addTransaction(newTransaction);
+  }
+
+  async function  addTransaction(transaction) {
+    // Logic to add transaction to the state or database
+    try {
+      const docRef = await addDoc(
+        collection(db, `users/${user.uid}/transaction`),
+        transaction
+      );
+      console.log("Document written with ID: ", docRef.id);
+
+      toast.success("Transaction Added!");
+    } catch (e) {
+      console.error("Error adding document:" , e);
+        toast.error("Couldn't add transaction");
+
+    }
   }
   return (
     <div>
